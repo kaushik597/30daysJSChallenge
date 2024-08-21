5d6db0c5423829860ff0299f3e4b4922fb969a3b
kaushik <kaushikyadavjallam005@gmail.com>
2024-08-20 23:23:43 -0400

    worked on menu items page

--- day28/script.js
+++ day28/script.js
@@ -1,6 +1,13 @@
 const ribbonContainer = document.querySelector("#ribbon-container");
 const carousel = document.querySelector("#carousel");
 const resContainer = document.querySelector("#res-container");
+const cuisineContainer = document.querySelector("#cuisine-container");
+const restaurantTitle = document.querySelector("#res-menu-title");
+const ratingContainer = document.querySelector("#rating-container");
+const feedbackContainer = document.querySelector("#feedback-container");
+const typeContainer = document.querySelector("#type-container");
+const categoryContainer = document.querySelector("#category-container");
+const menuItems = document.querySelector("#items");
 async function getData1() {
   try {
     const response = await fetch("data.json");
@@ -27,7 +34,7 @@ async function fetchDetails() {
 
   const ribbonData = data2.search_result.facet_list[3].values;
   const resDetails = data1.search_result.results;
-//   console.log(resDetails);
+  //   console.log(resDetails);
 
   ribbonData.map((item) => {
     let id = item.value;
@@ -38,24 +45,20 @@ async function fetchDetails() {
     createRibbonItem(id, src, alt, name);
   });
   carouselTransition();
-  resDetails.map(res=>{
+  resDetails.map((res) => {
     //resId,url,name,rating,time,price
-    let resId=res.restaurant_id;
-    let url= res.media_image.base_url+res.media_image.public_id
-    let name=res.name
-    let rating=`${res.ratings.rating_value} (${res.ratings.rating_count})`
-    let time=`${res.delivery_estimate_range.start_time_minutes} - ${res.delivery_estimate_range.end_time_minutes}`
-    let price=res.price_response.fee_display_setting.search_display_setting.styled_text.text
+    let resId = res.restaurant_id;
+    let url = res.media_image.base_url + res.media_image.public_id;
+    let name = res.name;
+    let rating = `${res.ratings.rating_value} (${res.ratings.rating_count})`;
+    let time = `${res.delivery_estimate_range.start_time_minutes} - ${res.delivery_estimate_range.end_time_minutes}`;
+    let price =
+      res.price_response.fee_display_setting.search_display_setting.styled_text
+        .text;
 
-    console.log("values",resId,url,name,rating,time,price);
-    createRestaurantCard(resId,url,name,rating,time,price)
-    
-  })
-
-
-  const menuItems= await fetch("https://api-gtm.grubhub.com/restaurants/457735/menu_items/?menuItemIds=17573388340").then(res=>res.json()).then(data=>console.log("menu items",data)
-  )
-
+    console.log("values", resId, url, name, rating, time, price);
+    createRestaurantCard(resId, url, name, rating, time, price);
+  });
 }
 
 function createRibbonItem(id, src, alt, name) {
@@ -99,19 +102,10 @@ function carouselTransition() {
   updateCarousel();
 }
 
-
-function createRestaurantCard(resId,url,name,rating,time,price){
-    const div= document.createElement("div");
-    // div.setAttribute("id",id)
-    // const img= document.createElement("img");
-    // img.setAttribute("src",src)
-    // img.setAttribute("alt",alt)
-    // div.appendChild(img);
-    // const name= document.createElement("h4");
-    // name.textContent=resname
-    // const star=document.createElement("span");
-    // star.textContent=rating
-    const resDiv=` <div id=${resId}>
+function createRestaurantCard(resId, url, name, rating, time, price) {
+  const div = document.createElement("div");
+  const resDiv = `<a href="menu.html">
+            <div id=${resId}>
             <img id="res-image" src=${url} alt="${name}">
             <div id="res-title">
                 <i
@@ -130,13 +124,116 @@ function createRestaurantCard(resId,url,name,rating,time,price){
             <div id="price">
                 <span>${price} delivery</span>
             </div>
-        </div>`
-        div.innerHTML=resDiv;
-        resContainer.appendChild(div)
+        </div>
+        </a>`;
+  div.innerHTML = resDiv;
+  resContainer.appendChild(div);
+}
+
+async function showCuisineData() {
+  const resData = await fetch("restaurantMenu.json");
+  const data = await resData.json();
+  const cuisineData = data.restaurant_data.restaurant.cuisines;
+  const title = data.restaurant_data.restaurant.name;
+  const rating = data.restaurant_data.restaurant.rating.rating_value;
+  const rating_count = data.restaurant_data.restaurant.rating.rating_count;
+  const address = data.restaurant_data.restaurant.address.street_address;
+  const phone = data.restaurant_data.restaurant.phone_number_for_delivery;
+  const goodFood = `${data.restaurant_data.restaurant.faceted_rating_data.faceted_rating_list[2].positive_response_count}% Good food `;
+  const onTimeDelivery = `${data.restaurant_data.restaurant.faceted_rating_data.faceted_rating_list[0].positive_response_count}% On time delivery `;
+  const correctOrder = `${data.restaurant_data.restaurant.faceted_rating_data.faceted_rating_list[1].positive_response_count}% Correct order `;
+  console.log(
+    "res data",
+    cuisineData,
+    title,
+    rating,
+    rating_count,
+    address,
+    phone,
+    goodFood,
+    onTimeDelivery,
+    correctOrder
+  );
+
+  cuisineData.map((cuisine) => {
+    const p = document.createElement("p");
+    p.textContent = `${cuisine},`;
+    cuisineContainer.appendChild(p);
+  });
+
+  const h1 = document.createElement("h1");
+  h1.textContent = title;
+  restaurantTitle.appendChild(h1);
+
+  const starRating = document.createElement("p");
+  starRating.textContent = rating;
+
+  const starRatingCount = document.createElement("p");
+  starRatingCount.textContent = rating_count;
+
+  const addressData = document.createElement("p");
+  addressData.textContent = address;
+
+  const phoneData = document.createElement("p");
+  phoneData.textContent = phone;
+
+  ratingContainer.appendChild(starRating);
+  ratingContainer.appendChild(starRatingCount);
+  ratingContainer.appendChild(addressData);
+  ratingContainer.appendChild(phoneData);
+
+  const good = document.createElement("p");
+  good.textContent = goodFood;
+  const ontime = document.createElement("p");
+  ontime.textContent = onTimeDelivery;
+  const correct = document.createElement("p");
+  correct.textContent = correctOrder;
+
+  feedbackContainer.appendChild(good);
+  feedbackContainer.appendChild(ontime);
+  feedbackContainer.appendChild(correct);
+
+  console.log("menu items", data.menu_items);
+
+  data.menu_items.map((item) => {
+    const itemCard = document.createElement("div");
+    itemCard.setAttribute("class", "item-card");
+
+    const img = document.createElement("img");
+    img.setAttribute(
+      "src",
+      item.media_image.base_url + item.media_image.public_id
+    );
+    img.setAttribute("alt", item.name);
+    img.setAttribute("class", "item-image");
+
+    itemCard.appendChild(img);
+
+    const namedDiv = document.createElement("div");
+    namedDiv.setAttribute("class", "item-details");
+    const name = document.createElement("p");
+    name.textContent = item.name;
+    namedDiv.appendChild(name);
+
+    const desc = document.createElement("p");
+    desc.textContent = item.description;
+    namedDiv.appendChild(desc);
+
+    const price = document.createElement("p");
+    price.textContent = item.maximum_price_variation.styled_text.text;
+    namedDiv.appendChild(price);
+    itemCard.appendChild(namedDiv);
+
+    const btn = document.createElement("button");
+    btn.setAttribute("id", item.name);
+    btn.textContent = "Add to cart";
+    namedDiv.appendChild(btn);
+
+    menuItems.appendChild(itemCard);
+  });
 }
 
 fetchDetails();
+showCuisineData();
 
-
-
-//completed till displaying res cards but should fix ui
+//completed till displaying menu items now i have to work on adding to cart and payment functionality
